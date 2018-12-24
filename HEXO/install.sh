409110cc0b862e0b04809c08c3686ac2d7e2326f
itxia <njuitman@163.com>
2018-12-24 22:33:47 +0800

    add install.sh

---
+++ HEXO/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+npm ls --depth 0
+npm install --save hexo
+npm install --save hexo-deployer-git
+npm install --save hexo-generator-archive
+npm install --save hexo-generator-category
+npm install --save hexo-generator-index-pin-top
+npm install --save hexo-generator-tag
+npm install --save hexo-renderer-ejs
+npm install --save hexo-renderer-marked
+npm install --save hexo-renderer-stylus
+npm install --save hexo-server
+npm ls --depth 0
